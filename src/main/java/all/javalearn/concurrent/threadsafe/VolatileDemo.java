9fc0b8b3b822f1ff3177a05e37919fa20c7dc512
juzi <shudebao@juzifenqi.com>
2018-12-03 17:57:48 +0800

    vilatile

--- src/main/java/all/javalearn/concurrent/threadsafe/VolatileDemo.java
+++ src/main/java/all/javalearn/concurrent/threadsafe/VolatileDemo.java
@@ -18,30 +18,21 @@ public class VolatileDemo
 
     public static void main(String[] args) throws InterruptedException {
         final VolatileDemo volatileDemo = new VolatileDemo();
+        CountDownLatch countDownLatch = new CountDownLatch(10000);
+
         for (int i = 0; i < 10000; i++) {
-            CountDownLatch countDownLatch = new CountDownLatch(2);
             new Thread(new Runnable() {
-                @Override
                 public void run() {
                     volatileDemo.addOne();
                     System.out.println("线程" + Thread.currentThread().getName() + "执行完毕!");
                     countDownLatch.countDown();;
                 }
             }).start();
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    volatileDemo.addOne();
-                    System.out.println("线程" + Thread.currentThread().getName() + "执行完毕!");
-                    countDownLatch.countDown();;
-                }
-            }).start();
-
-            countDownLatch.await();
         }
 
-        //如果没有volatile关键字修饰i，打印出来19998，19999,20000等
+        countDownLatch.await();
+
+        //无论有没有volatile关键字修饰i，打印出来9998，9999,10000等
         System.out.println(volatileDemo.i);
 
     }
