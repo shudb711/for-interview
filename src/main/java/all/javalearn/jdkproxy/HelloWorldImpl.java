3e7a8eda293f6f02f980bbfeacf53325427feb39
juzi <shudebao@juzifenqi.com>
2019-01-19 14:46:12 +0800

    jdk动态代理

--- src/main/java/all/javalearn/jdkproxy/HelloWorldImpl.java
+++ src/main/java/all/javalearn/jdkproxy/HelloWorldImpl.java
@@ -6,6 +6,8 @@ package all.javalearn.jdkproxy;
  * @Date:2018/10/11 10:43
  **/
 public class HelloWorldImpl implements HelloWorld {
+//    public final void hello() {
+//    final方法也可以（jdk动态代理）
     public void hello() {
         System.out.println("=====hello world=====");
     }
--- src/main/java/all/javalearn/jdkproxy/ProxyTest.java
+++ src/main/java/all/javalearn/jdkproxy/ProxyTest.java
@@ -3,13 +3,14 @@ package all.javalearn.jdkproxy;
 import java.lang.reflect.Proxy;
 
 /**
- * @Function:
+ * @Function:jdk动态代理
  * @Author:shudb
  * @Date:2018/10/11 10:45
  **/
 
 public class ProxyTest {
     public static void main(String[] args) {
+        //打开保存开关
         System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
 
         HelloWorldInvocationHandler handler = new HelloWorldInvocationHandler(new HelloWorldImpl());
@@ -17,6 +18,8 @@ public class ProxyTest {
                 ProxyTest.class.getClassLoader(),
                 new Class[]{HelloWorld.class},
                 handler);
+        //获取生成代理类的路径
+        System.out.println("$Proxy0.class全名: "+Proxy.getProxyClass(HelloWorld.class.getClassLoader(), HelloWorld.class));
         proxy.hello();
     }
 }
