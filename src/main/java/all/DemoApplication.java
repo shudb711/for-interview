cae772bfc2006a2713c0f0cf02a6bd51757decc5
shudebao <shudebao@weshare.com.cn>
2019-04-23 10:51:27 +0800

    merge

--- src/main/java/all/DemoApplication.java
+++ src/main/java/all/DemoApplication.java
@@ -11,6 +11,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("111222333444");
+		System.out.println("111");
+		System.out.println("222");
+		System.out.println("333");
+		System.out.println("444");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
