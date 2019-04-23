9636832207b7c7cd9dd9b01272199b277fc868b9
shudebao <shudebao@weshare.com.cn>
2019-04-23 10:39:36 +0800

    111

--- src/main/java/all/DemoApplication.java
+++ src/main/java/all/DemoApplication.java
@@ -11,7 +11,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("111222333444");
-		System.out.println("111");
 		System.out.println("222");
 		System.out.println("333");
 		System.out.println("444");
