a9c89e3aa10115ede39226a6e5b3d5288cca722a
shudebao <shudebao@weshare.com.cn>
2019-04-23 10:31:54 +0800

    test2

--- src/main/java/all/DemoApplication.java
+++ src/main/java/all/DemoApplication.java
@@ -12,6 +12,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("111222333444");
 		System.out.println("111");
+		System.out.println("222");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
