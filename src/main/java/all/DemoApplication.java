a920a813a8042677db2135e92587576a1e097102
shudebao <shudebao@weshare.com.cn>
2019-05-08 14:27:38 +0800

    commit333

--- src/main/java/all/DemoApplication.java
+++ src/main/java/all/DemoApplication.java
@@ -12,6 +12,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println(111);
 		System.out.println(222);
+		System.out.println(333);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
