067d59868b5042851ff6c1107ca8d56d4a18e826
shudebao <shudebao@weshare.com.cn>
2019-05-09 14:59:14 +0800

    commit 444

--- src/main/java/all/DemoApplication.java
+++ src/main/java/all/DemoApplication.java
@@ -13,6 +13,8 @@ public class DemoApplication {
 		System.out.println(111);
 		System.out.println(222);
 		System.out.println(333);
+
+		System.out.println(444);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
