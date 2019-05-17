2bcba6d2ba7026f0399ee8204660f43cb1e8f502
shudebao <shudebao@weshare.com.cn>
2019-05-17 14:08:02 +0800

    eeeeeeeee

--- src/main/java/all/DemoApplication.java
+++ src/main/java/all/DemoApplication.java
@@ -14,9 +14,6 @@ public class DemoApplication {
 		System.out.println(222);
 		System.out.println(333);
 
-		System.out.println(444);
-		System.out.println(555);
-		System.out.println(666);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
