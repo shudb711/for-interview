39f6af51288a97cc9fe2c296781a5eb157727da4
shudebao <shudebao@tal.com>
2022-01-21 10:14:06 +0800

    array

--- src/main/java/all/javalearn/datastructure/arithmetic/ArrayTest.java
+++ src/main/java/all/javalearn/datastructure/arithmetic/ArrayTest.java
@@ -8,6 +8,10 @@ package all.javalearn.datastructure.arithmetic;
 public class ArrayTest {
 
     /**
+     * 1、前缀和：主要适⽤的场景是原始数组不会被修改的情况下，频繁查询某个区间的累加和
+     */
+
+    /**
      * 力扣303
      * 计算以为数组 i到j的 元素和
      * 前缀和（一维数组）,用一个数组保存前n个数之和，sum()方法直接操作该数组即可，时间复杂度是O(1),之后如果多次调用sum()方法的话，可以提高效率，不必每次都重新计算
@@ -43,7 +47,7 @@ public class ArrayTest {
             preSum = new int[m + 1][n + 1];
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
-                    preSum[i + 1][j + 1] = preSum[i][j + 1] + preSum[i + 1][j] -preSum[i][j];
+                    preSum[i + 1][j + 1] = preSum[i][j + 1] + preSum[i + 1][j] + matrix[i][j] - preSum[i][j];
                 }
             }
         }
@@ -85,4 +89,59 @@ public class ArrayTest {
             return sum;
         }
     }
+
+    /**
+     * 2、差分数组：主要适⽤场景是频繁对原始数组的某个区间的元素进⾏增减。
+     */
+
+    class Difference{
+        private int[] diff;
+
+        public Difference(int[] arr) {
+            assert arr != null && arr.length > 0;
+            diff = new int[arr.length];
+            diff[0] = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                diff[i] = arr[i] - arr[i -1];
+            }
+        }
+
+        public void increment(int i, int j, int inc) {
+            diff[i] += inc;
+            if (j + 1 < diff.length) {
+                diff[j + 1] -= inc;
+            }
+        }
+
+        public int[] getResult() {
+            int[] res = new int[diff.length];
+            res[0] = diff[0];
+            for (int i = 1; i < diff.length; i++) {
+                res[i] = res[i - 1] + diff[i];
+            }
+            return res;
+        }
+
+
+
+
+        /**
+         * 力扣370：区间加法
+         * 给一个长度为n的数组，初始情况下值都是0，你将会被给出k个操作，每个操作表示为一个三元组，[startIndex,endIndex,inc]，
+         * 你需要将子数组A[startIndex...endIndex](包含startIndex和endIndex)增加inc,请你返回k次操作之后的数组
+         * 例如：输入:length=5,updates=[[1,3,2],[2,4,3],[0,2,-2]],输出[-2,0,3,5,3]
+         * 解释：初始状态[0,0,0,0,0],操作[1,3,2]后，变为：[0,2,2,2,0],操作[2,4,3]后，变为[0,2,5,5,3]，操作[0,2,-2]后，变为[-2,0,3,5,3]
+         */
+        public int[] getModifiedArray(int length, int[][] updates) {
+            for (int i = 0; i < updates.length; i++) {
+                int[] update = updates[i];
+                int startIndex = update[0];
+                int endIndex = update[1];
+                int inc = update[2];
+                increment(startIndex, endIndex, inc);
+            }
+            return getResult();
+        }
+    }
+
 }
